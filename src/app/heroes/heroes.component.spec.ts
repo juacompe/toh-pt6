8b891159ac51b1e5cc3f870d9af0c69682b08180
juacompe <juacompe@gmail.com>
2018-09-06 17:45:08 +0700

    :recycle: Introduce HeroPresenter
    Video 1: Presenter pattern in Angular Hero App
    Introduce HeroPresenter class
    Move add logic from compenent into the presenter
    write unit tests for add logic
    1 test still fail

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,7 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HeroesComponent } from './heroes.component';
+import { HeroesComponent, HeroesPresenter, View } from './heroes.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Observable } from 'rxjs';
+import { Hero } from '../hero';
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
@@ -25,3 +27,40 @@ describe('HeroesComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+describe('HeroesPresenter', () => {
+  let p
+  let view
+  beforeEach(() => {
+    view = new MockView()
+    p = new HeroesPresenter(view)
+    p.heroes = []
+  })
+  describe('add', () => {
+    it('should not add anonymous hero', () => {
+      p.add(' ')
+      expect(p.heroes.length).toEqual(0)
+    })
+
+    it('should call addHeroToService of the view', () => {
+      p.add('Jua')
+      view.verify()
+    })
+
+    it('should add new hero to list of heroes', () => {
+      p.add('Jua')
+      expect(p.heroes.length).toEqual(1)
+    })
+  })
+});
+
+class MockView implements View {
+  called = false
+  addHeroToService(hero: Hero): Observable<Hero> {
+    this.called = true
+    return new Observable()
+  }
+  verify(): void {
+    expect(this.called).toBeTruthy()
+  }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,16 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
-  heroes: Hero[];
+export class HeroesComponent implements OnInit, View {
+  presenter: HeroesPresenter;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {
+    this.presenter = new HeroesPresenter(this)
+  }
 
   ngOnInit() {
     this.getHeroes();
@@ -19,21 +22,35 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.presenter.heroes = heroes);
   }
+
+  addHeroToService(hero: Hero): Observable<Hero> {
+    return this.heroService.addHero(hero)
+  }
+
+  delete(hero: Hero): void {
+    this.presenter.heroes = this.presenter.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
+}
+
+export interface View {
+  addHeroToService(hero: Hero): Observable<Hero>
+}
+
+export class HeroesPresenter {
+  heroes: Hero[];
+
+  constructor(private view: View) {}
 
   add(name: string): void {
     name = name.trim();
     if (!name) { return; }
-    this.heroService.addHero({ name } as Hero)
+    this.view.addHeroToService({ name } as Hero)
       .subscribe(hero => {
         this.heroes.push(hero);
       });
   }
-
-  delete(hero: Hero): void {
-    this.heroes = this.heroes.filter(h => h !== hero);
-    this.heroService.deleteHero(hero).subscribe();
-  }
-
 }
